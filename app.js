caab5de44e3705c9b817e92780a023b43f6be155
Jason Leibowitz <jasonaleibowitz@gmail.com>
2015-09-28 15:29:00 -0400

    Heroku port resolver

--- app.js
+++ app.js
@@ -51,9 +51,38 @@ app.get('/open', function(req, res) {
 	openKisi();
 });
 
-
-var server = app.listen(3000, function() {
-	var port = server.address().port;
-
-	console.log('Kisi Workflow API app started on localhost://%s', port);
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+var port = normalizePort(process.env.PORT || '3000');
+
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
