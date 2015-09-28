9b655139fb7aaa3eecd76764c5ba1ea2f0175ffe
Jason Leibowitz <jasonaleibowitz@gmail.com>
2015-09-28 16:31:29 -0400

    got it working

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require('express');
+var path = require('path');
 var http = require('http');
 var request = require('request');
 var unirest = require('unirest');
 var app = express();
 
-function openKisi() {
-	var options = {
-		method		: 'POST',
-  		url			: 'http://www.kisi.de/locks/1144/access.json',
-		headers		:	{
-			'x-authentication-token'	: '92ec5f0786810e611f826dcb53b9bb9f',
-     		'content-type'				: 'application/json'
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+app.get('/open', function(req, res) {
+	request({
+		url: "https://www.kisi.de/locks/1144/access.json",
+		method: "POST",
+		headers: {
+			"x-authentication-token": req.query.auth
 		},
-  		body		: JSON.stringify({
+		body: {
 			"lock": {
 				"channel": null,
 				"created_at": "2014-10-17T20:48:44.000Z",
@@ -32,26 +39,32 @@ function openKisi() {
 					"share": false
 				}
 			}
-		})
-	};
-
-	request(options, function(error, response, body) {
-		if (error) throw new Error(error);
-		console.log('statusCode: ', response.statusCode);
-		console.log('body: ', body);
-		return response.statusCode;
+		},
+		json: true
+	}, function(error, response, body){
+		if (error) {
+			throw new Error(error);
+			res.send('error');
+		};
+		if (response.statusCode == 200 && body.message == "Unlocked!") {
+			res.send('unlocked');
+		}
 	})
+});
+
+///////////////////////////////////////////////
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
-
-app.get('/', function(req, res) {
-	res.send('Hello World');
-});
-
-app.get('/open', function(req, res) {
-	// var authCode = req.query.auth;
-	var statusCode = openKisi();
-	res.send(statusCode);
-});
 
 // production error handler
 // no stacktraces leaked to user
